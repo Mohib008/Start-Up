a3e50d59fcae3f8b7d1a459a1ab9c1fb083aeaf4
Mohib <mohib.0008@gmail.com>
2019-07-01 17:24:14 -0400

    done one

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 
 
-app.get("home", (req, res) => {
+app.get("/", (req, res) => {
     res.render("home");
 });
 app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + "/css"));
-app.use(express.static(__dirname + "/img"));
+app.use(express.static(__dirname + "/"));
+app.use(express.static(__dirname + "/"));
 app.use(express.static(__dirname + "/sass"));
 app.use(express.static(__dirname + "/views"));
 
