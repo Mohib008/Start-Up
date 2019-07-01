45be559385161dccd355b6e656c37b4b4681b1b1
Mohib <mohib.0008@gmail.com>
2019-07-01 16:22:13 -0400

    edit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
-app.get("/home", (req, res) => {
+app.get("home", (req, res) => {
     res.render("home");
 });
 
