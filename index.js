5e812eb003abde8d67ecf8c601211bfdf3053248
Mohib <mohib.0008@gmail.com>
2019-07-01 16:46:03 -0400

    done tow

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
     res.render("home");
 });
+app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + "./css"));
-app.use(express.static(__dirname + "./img"));
-app.use(express.static(__dirname + "./sass"));
-app.use(express.static(__dirname + "./views"));
+app.use(express.static(__dirname + "/css"));
+app.use(express.static(__dirname + "/img"));
+app.use(express.static(__dirname + "/sass"));
+app.use(express.static(__dirname + "/views"));
 
 
 app.listen(process.env.PORT || 3000, () => {
