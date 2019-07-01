a97699fc4957123bdab0e54bd2e1dda9cffe0438
Mohib <mohib.0008@gmail.com>
2019-07-01 16:27:59 -0400

    done

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/views"));
-app.use(express.static(__dirname + "/css"));
-app.use(express.static(__dirname + "/sass"));
-app.use(express.static(__dirname + "/img"));
+app.use(express.static(__dirname + "views"));
+app.use(express.static(__dirname + "css"));
+app.use(express.static(__dirname + "sass"));
+app.use(express.static(__dirname + "img"));
 
 
 app.listen(process.env.PORT || 3000, () => {
